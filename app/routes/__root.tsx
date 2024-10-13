75bdfb84a56f5017b7e056f73ece013d361b7911
Andy Quinn <andy.kettler.quinn@gmail.com>
2024-10-13 13:19:02 -0500

    initial commit

---
+++ app/routes/__root.tsx
@@ -0,0 +1,46 @@
+import { createRootRoute } from "@tanstack/react-router"
+import { Outlet, ScrollRestoration } from "@tanstack/react-router"
+import { Body, Head, Html, Meta, Scripts } from "@tanstack/start"
+import * as React from "react"
+
+export const Route = createRootRoute({
+  meta: () => [
+    {
+      charSet: "utf-8",
+    },
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
+    },
+    {
+      title: "TanStack Start Starter",
+    },
+  ],
+  component: RootComponent,
+  notFoundComponent: () => {
+    return <p>Requested route does not exist</p>
+  },
+})
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  )
+}
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <Html>
+      <Head>
+        <Meta />
+      </Head>
+      <Body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </Body>
+    </Html>
+  )
+}
